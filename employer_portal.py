a45b8ad5b3938b4a9ef22923d6779cf877f6693c
JiviteshG <gudekar.jivitesh@gmail.com>
2019-04-12 02:42:17 -0300

    Added employer files and modified

--- employer_portal.py
+++ employer_portal.py
@@ -4,10 +4,25 @@ import requests
 import json
 import logging
 import xml.etree.ElementTree as ET
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+from flask_dance.contrib.github import make_github_blueprint, github 
+import os 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sneha3010@localhost/projectdb'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+# Source: https://stackoverflow.com/questions/27785375/testing-flask-oauthlib-locally-without-https
+# for github HTTPS
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+twitter_blueprint = make_twitter_blueprint(api_key='snajatcEGgC19dwf9AFNQMtM8 ', api_secret='ieX5hO4amP6ay6bkPCyY1Bfla9BtWMla7NWRN9nyzu4tDdVKR4')
+github_blueprint = make_github_blueprint(client_id='b1198cf45b0983ee621f', client_secret='80ad32fdf60fc3350e398da866e77891075d15b4')
+
+app.register_blueprint(twitter_blueprint, url_prefix = '/twitter_login')
+app.register_blueprint(github_blueprint, url_prefix = '/github_login')
+
 db = SQLAlchemy(app)
 
 employee_id = '1'
@@ -23,7 +38,22 @@ class Employee_details(db.Model):
 
 @app.route('/')
 def home(): 
-	return render_template('home.html')
+# if github.authorized:
+	print("A")
+	account_info = github.get('/user')
+	print(account_info)
+	git_username = ''
+	print(git_username)
+	print('Checking uname')
+	if account_info.ok:
+		print('Checking uname started')
+	account_info_json = account_info.json()
+	git_username = account_info_json['login']
+	print(git_username)
+	session['user'] = git_username
+	print("Reach")
+	return redirect(url_for('addEmployer',  username=session['user']))
+	return render_template('employer_home.html')
 
 @app.before_request
 def before_request():
@@ -32,51 +62,111 @@ def before_request():
 		g.user = session['user']
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+# @app.route('/github')
+# def github_login():	
+# 	account_info = github.get('/user')
+# 	print('4')
+# 	if account_info.ok:
+# 		account_info_json = account_info.json()
+
+# 		return '<h1> Your GitHub name is @{}'.format(account_info_json['login'])
+
+
+@app.route('/github_login', methods=['GET', 'POST'])
+def github_login():
+
+	
+
+	session.pop('user', None)
+	# print('1')
+	# if not twitter.authorized:
+	# 	print('2')
+	# 	return redirect(url_for('twitter.login'))
+	# print('3')
+	# account_info = twitter.get('account/settings')
+	# print('4')
+	# if account_info.ok:
+	# 	account_info_json = account_info.json()
+
+	# 	return '<h1> Your Twitter name is @{}'.format(account_info_json['screen_name'])
+
+	# Correct code
+	# if not github.authorized:
+	# 	return redirect(url_for('github.login'))
+	# account_info = github.get('/user')
+	# git_username = ''
+	# if account_info.ok:
+	# 	account_info_json = account_info.json()
+	# 	git_username = account_info_json['login']
+
+
+
 	f = open("log1.txt", "a+")
 	f.write("method: GET \nEnd-point: http://127.0.0.1:8001/login \nparameters: None\n" )
 	f.close()
 	# Employee_details1 = Employee_details.query.filter_by(username=username),first()
 	
-	session.pop('user', None)
+	
 
 	if request.method == 'POST':
 		f = open("log1.txt", "a+")
-		f.write("method: POST \nEnd-point: http://127.0.0.1:8001/login \nparameters: username: "+request.form['username']+", password:  ")
+		f.write("method: POST \nEnd-point: http://127.0.0.1:8001/login \nparameters: Login from git ")
 		f.close()
+
+		### CODE FOR OAUTH
+		print("Reach0")
+		if not github.authorized:
+			print("NA")
+			return redirect(url_for('github.login'))
+		print("A")
+		account_info = github.get('/user')
+		print(account_info)
+		git_username = ''
+		print(git_username)
+		print('Checking uname')
+		if account_info.ok:
+			print('Checking uname started')
+			account_info_json = account_info.json()
+			git_username = account_info_json['login']
+			print(git_username)
+			session['user'] = git_username
+			print("Reach")
+			return redirect(url_for('addEmployer',  username=session['user']))
+
+		## MAINSTREAM LOGIN
 		# for i in range(1):
 		# 	f.write("POST, http://127.0.0.1:8001/login, username: "+request.form['username']+", password: "+request.form['password']+" %d\r\n" % (i))
-		emppass = request.form
-		username = request.form['username']
-		get_emp = Employee_details.query.filter_by(username=username).first()
-		password = request.form['password']
+		# emppass = request.form
+		# username = request.form['username']
+		# get_emp = Employee_details.query.filter_by(username=username).first()
+		# password = request.form['password']
 
 
-		pathToXML = "Salt.xml"
-		tree = ET.parse(pathToXML)
-		root = tree.getroot()
+		# pathToXML = "Salt.xml"
+		# tree = ET.parse(pathToXML)
+		# root = tree.getroot()
 
-		# all items data
-		print('Expertise Data:'+str(root)+str(tree))
-		salt = ''
-		for elem in root:
-			salt = elem.text
+		# # all items data
+		# print('Expertise Data:'+str(root)+str(tree))
+		# salt = ''
+		# for elem in root:
+		# 	salt = elem.text
 
 
-		if get_emp is None:
-			error = 'User not present! Try again.'
-			return render_template('login.html', error = error)
-		if  get_emp.password != password+salt:
-			error = 'Password do not match! Try again.'
-			return render_template('login.html', error = error)
-		else:
-			session['user'] = username
-			return redirect(url_for('addEmployer',  username=session['user']))
+		# if get_emp is None:
+		# 	error = 'User not present! Try again.'
+		# 	return render_template('login.html', error = error)
+		# if  get_emp.password != password+salt:
+		# 	error = 'Password do not match! Try again.'
+		# 	return render_template('login.html', error = error)
+		# else:
+		# session['user'] = username
+		# return redirect(url_for('addEmployer',  username=session['user']))
 		
 		
-
-	return render_template('login.html')
+	# return '<h1>Request failed! <h1>'
+	# return render_template('login.html')
+	return render_template('github_login.html')
 
 
 @app.route('/employer_form/<string:username>', methods=['POST','GET'])
@@ -86,10 +176,11 @@ def addEmployer(username):
 	f.close()
 
 
-	if g.user == username and request.method == 'POST':
-		f = open("log1.txt", "a+")
-		f.write("method: POST \nEnd-point: http://127.0.0.1:8001//employer_form/<string:username> \nparameters: username: "+request.form['username']+", application_no: "+request.form['application_no']+", mbr_web_service: "+request.form['mbr_web_service']+" ")
-		f.close()
+	if request.method == 'POST':
+		print(g.user)
+		# f = open("log1.txt", "a+")
+		# f.write("method: POST \nEnd-point: http://127.0.0.1:8001//employer_form/<string:username> \nparameters: username: "+request.form['username']+", application_no: "+request.form['application_no']+", mbr_web_service: "+request.form['mbr_web_service']+" ")
+		# f.close()
 
 		employee = Employee_details()
 		employerDetails = request.form
@@ -98,17 +189,19 @@ def addEmployer(username):
 		mbr_web_service_address = employerDetails["mbr_web_service"]
 
 		get_employee = Employee_details.query.filter_by(username=g.user).first()
-
+		url = str(mbr_web_service_address)+'?salary='+str(get_employee.salary)+'&application_number='+str(application_number)+'&emp_name='+str(get_employee.emp_name)+'&emp_start_date='+str(get_employee.emp_start_date)
+		print(url)
 		try:
+
 			r = requests.get(str(mbr_web_service_address)+'?salary='+str(get_employee.salary)+'&application_number='+str(application_number)+'&emp_name='+str(get_employee.emp_name)+'&emp_start_date='+str(get_employee.emp_start_date))
 		except:
 			message = 'Invalid endpoint. try again'
-			return render_template('updatestatus.html', message=message)
+			return render_template('employer_updatestatus.html', message=message)
 
 		if r.text == 'success':
 
 			message = 'Employee details submitted sucessfully to MBR portal.'
-			return render_template('updatestatus.html', message=message)
+			return render_template('employer_updatestatus.html', message=message)
 		else:
 
 	 		return "<h1> Error occured while submitting details to MBR portal. </h1>"
@@ -122,7 +215,7 @@ def addEmployer(username):
 			return render_template('employer_form.html', username=username)
 
 
-	return redirect(url_for('login'))
+	return redirect(url_for('github.login'))
 
 @app.route('/logout', methods=['GET'])
 def logout():
@@ -130,7 +223,8 @@ def logout():
 	f.write("method: GET \nEnd-point: http://127.0.0.1:8001/logout \nparameters: logged out\n" )
 	f.close()
 	session.pop('user', None)
-	return redirect(url_for('login'))
+	# session.pop('user', None)
+	return redirect(url_for('github_login'))
 
 if __name__ == '__main__':
 	app.secret_key = 'abcdweb'
